dd18f64ba676d4c8074d2521c9f636059503863f
eduardusoktario <58355146+eduardusoktario@users.noreply.github.com>
2024-07-19 05:24:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from streamlit_option_menu import option_menu
